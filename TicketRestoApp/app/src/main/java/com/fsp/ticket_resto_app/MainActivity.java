4f116885b8f8d416cfb95ebfd501a0148262414a
Francois SP <francois-sp@gmx.fr>
2023-09-30 17:27:32 +0200

    La vue ce lance

--- TicketRestoApp/app/src/main/java/com/fsp/ticket_resto_app/MainActivity.java
+++ TicketRestoApp/app/src/main/java/com/fsp/ticket_resto_app/MainActivity.java
@@ -1,14 +1,33 @@
 package com.fsp.ticket_resto_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new Accueil(), "Accueil");
+        vpAdapter.addFragment(new Parametre(), "Paramètre");
+
+        viewPager.setAdapter(vpAdapter);
     }
 }
---
+++ TicketRestoApp/app/src/main/java/com/fsp/ticket_resto_app/VPAdapter.java
@@ -0,0 +1,43 @@
+package com.fsp.ticket_resto_app;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class VPAdapter extends FragmentPagerAdapter {
+
+    private final ArrayList<Fragment> fragments = new ArrayList<>();
+    private final ArrayList<String> titles = new ArrayList<>();
+
+    public VPAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return (fragments.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragments.add(fragment);
+        titles.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles.get(position);
+    }
+
+
+}
