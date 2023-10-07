5292145c40f4a9032fa967fbea06448bf28a62cc
Francois389 <francois-sp@gmx.fr>
2023-10-07 16:06:27 +0200

    Fix id

---
+++ TicketRestoApp/app/src/main/java/com/fsp/ticket_resto_app/controlleur/Controlleur.java
@@ -0,0 +1,66 @@
+/*
+ * Controlleur.java                                   20 août 2023
+ * François de Saint Palais, aucun droit réservé
+ */
+
+package com.fsp.ticket_resto_app.controlleur;
+
+import com.fsp.ticket_resto_app.donnee.mathematique.Decomposition;
+
+/**
+ * Gére les interactions de l'utilisateur avec l'application.
+ * @autor François de Saint Palais
+ */
+public class Controlleur {
+
+    private static Controlleur instance = null;
+
+    private Decomposition decomposition;
+
+    /**
+     * Constructeur privé pour le singleton.
+     */
+    private Controlleur() {
+        super();
+        decomposition = new Decomposition();
+    }
+
+    public static Controlleur getControlleur() {
+        if (instance == null) {
+            instance = new Controlleur();
+        }
+        return instance;
+    }
+
+    public void setMontantAPayer(int montant) {
+        decomposition.setMontant(montant);
+    }
+
+    public int getQuantiteTicket1() {
+        return decomposition.decomposeNbPremierValeur();
+    }
+
+    public int getQuantiteTicket2() {
+        return decomposition.decomposeNbDeuxiemeValeur();
+    }
+
+    public int getReste() {
+        return decomposition.decomposeReste();
+    }
+
+    public void setValeurTicket1(int valeur) {
+        decomposition.setPremiereValeur(valeur);
+    }
+
+    public void setValeurTicket2(int valeur) {
+        decomposition.setDeuxiemeValeur(valeur);
+    }
+
+    public int getValeurTicket1() {
+        return decomposition.getPremiereValeur();
+    }
+
+    public int getValeurTicket2() {
+        return decomposition.getDeuxiemeValeur();
+    }
+}
